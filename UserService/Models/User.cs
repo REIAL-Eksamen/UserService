51fe2c397ae8d7945d7d04114b82407c031730fc
ennik <enni.koo@hotmail.com>
2026-05-15 12:47:38 +0200

    changes to models

--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -1,10 +1,11 @@
 namespace UserService.Models;
+
 public class User
 {
     public Guid UserId { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
     public string? PhoneNumber { get; set; }
     public RoleType Role { get; set; }
     public MembershipStatus MembershipStatus { get; set; }

3f45629f1d1d229fe06f226f3015db68fd753dd3
ennik <enni.koo@hotmail.com>
2026-05-25 18:49:27 +0200

    Færdig med modelklass comments

--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -2,12 +2,15 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace UserService.Models;
-
+// User modelklasse repræsenterer et medlem i FitLife-systemet.
+// Klassen bruges både til at gemme brugerdata i MongoDB og til at sende brugerdata via API'et.
 public class User
 {
+    //MongoDB bruger ObjectId som standard. BsonId fortæller mongo,at dette er dokumentets primære id.
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
+    //authId bruges til at koble brugeren smmen med et login/auth-system
     public string? AuthId { get; set; }
 
     public required string FirstName { get; set; }
@@ -18,14 +21,14 @@ public class User
     public MembershipStatus MembershipStatus { get; set; }
     public DateTime TimeCreated { get; set; } = DateTime.UtcNow;
 }
-
+//membership-type beskriver de medlemskabtyper vi tilbyder.
 public enum MembershipType
 {
     Student,
     Standard,
     Premium
 }
-
+//memberShip-status bruges til at vise, om en bruger har et aktivt medlemskab eller ej.
 public enum MembershipStatus
 {
     Active,
